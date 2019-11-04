4b9977efd312b64d660c5ffdba75fce8212abfb7
Bingoch <chenbingo2017@gmail.com>
2019-11-04 17:10:27 +0800

    update blog files

--- blog/deploy.sh
+++ blog/deploy.sh
@@ -4,7 +4,7 @@ hexo generate
 cp -R public/* deploy/Bingoch.github.io
 cd deploy/Bingoch.github.io
 git remote remove origin
-git remote add git@github.com:Bingoch/Bingoch.github.io.git
+git remote add origin git@github.com:Bingoch/Bingoch.github.io.git
 git add .
 git commit -m "update"
 git push origin master
@@ -12,7 +12,7 @@ git push origin master
 # 同步项目到github
 cd ../../..
 git remote remove origin
-git remote add git@github.com:Bingoch/hexo.git
+git remote add origin git@github.com:Bingoch/hexo.git
 git add .
 git commit -m "update blog files"
 git push origin master
